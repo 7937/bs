53dafb6cccab62dd21d01e97659995ca8dce8b52
Jinru Wang <a7937@7937air.local>
2018-04-17 15:37:08 +0800

    qrcode

--- replyMsg/views.py
+++ replyMsg/views.py
@@ -2,8 +2,14 @@
 from .models import Question
 from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponse
+from wxpy import *
+import time
+
 
 def question(request):
+    bot = Bot(cache_path=True,qr_path=/static/qr.png)
+    time.sleep(3)
+    
     questions = Question.objects.all()
     context = {}
     context['questions'] = questions
