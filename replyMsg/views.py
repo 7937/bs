8a76282a789fbca2d36baf72e141c9b91173f205
Jinru Wang <a7937@7937air.local>
2018-04-19 13:59:33 +0800

    1.1

--- replyMsg/views.py
+++ replyMsg/views.py
@@ -11,20 +11,13 @@ import threading
 def login_c():
     print "login in"
 
-def qr():
-    print "------get qr finished------"
-
 def question(request):
-    #try:
-   # bot = Bot(cache_path=True,login_callback=login_c)
-   # bot = Bot(console_qr=2)
-   
-    # logging.debug("A log message")	
-    #itchat.auto_login(enableCmdQR=2,picDir="../static/img/qr.png")
-    #except Exception,e:
-     #   print e
+    #bot = Bot(cache_path=True,qr_path="../static/img/qr.png",login_callback=login_c)
+    bot = Bot(cache_path=True,console_qr=True)
+    tuling = Tuling(api_key='884ea5d4a41d48f583649eb8ea443669')
+    
+    logging.debug("A log message")	
 
-    threading.Thread(target=doBot).start()
     time.sleep(2)
 
     questions = Question.objects.all()
@@ -34,25 +27,15 @@ def question(request):
     context['questions'] = questions
     context['auto'] = auto
   
-    return render_to_response('question.html',context)  
-#    threading.Thread(target=doHmtl).start()
-
     for a in auto:
         if a.auto == True:
-#            @bot.register()
+            @bot.register()
             def reply_my_friend(msg):
                 tuling.do_reply(msg)
         else:
- #           @bot.register()
+            @bot.register()
             def auto_reply(msg):
                 for question in questions:
                     if isinstance(msg.chat, Friend) & question.content==msg.text:
                         return ' {} '.format(msg.text)
-
-def doBot():
-    tuling = Tuling(api_key='884ea5d4a41d48f583649eb8ea443669')
-    bot = Bot(cache_path=True,qr_path="../static/img/qr.png",login_callback=login_c)
-
-
-def doHmtl():
-    return render_to_response('question.html',context)
+    return render_to_response('question.html',context)  
